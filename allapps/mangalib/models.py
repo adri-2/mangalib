f422948a3b13fc5c03c8fc2944f299c7f3c546ce
adr-2 <www.adriensasni@gmail.com>
2025-01-12 21:45:46 +0100

    modife du .env du backend

--- allapps/mangalib/models.py
+++ allapps/mangalib/models.py
@@ -13,10 +13,12 @@ class Livre(models.Model):
     titre = models.CharField(max_length = 32, unique=True,verbose_name="Titre")
     quantity = models.IntegerField(default=1,verbose_name="Quantité")
     auteur = models.ForeignKey(Auteur, on_delete=models.DO_NOTHING,verbose_name="Auteur")
-    #date = models.DateField(verbose_name="Date de diffusion")
+    # date = models.DateField(verbose_name="Date de diffusion")
     class Meta:
         verbose_name ="Livre"
         verbose_name_plural ="Livres"
     def __str__(self):
         return self.titre
 
+class Categories(models.Model):
+    name = models.CharField(max_length = 32, unique=True,verbose_name="Titre")
